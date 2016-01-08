7a2f560d08928de348efbeeae211c69a6351af93
shoemonkey <romanr32@gmail.com>
2016-01-08 14:49:44 -0600

    Security/Login page linked

--- src/main/java/springboot/security/config/SecurityConfig.java
+++ src/main/java/springboot/security/config/SecurityConfig.java
@@ -1,23 +1,63 @@
 package springboot.security.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled=true)
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
-	@Autowired
-	public void configGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-			.inMemoryAuthentication()
-				.withUser("admin").password("password").roles("ADMIN");
-	}
+	//initial configuration which worked
+//	@Autowired
+//	public void configGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//		auth
+//			.inMemoryAuthentication()
+//				.withUser("admin").password("password").roles("ADMIN");
+//	}
 	
 //	@Override
 //	public void configure(HttpSecurity httpSecurity) throws Exception {
 //		httpSecurity.authorizeRequests().antMatchers("/").permitAll();
 //	}
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/", "/public/**").permitAll()
+				.antMatchers("/users/**").hasAuthority("ADMIN")
+				.anyRequest().fullyAuthenticated()
+				.and()
+				.formLogin()
+				.loginPage("/login")
+				.failureUrl("/login?error")
+				.usernameParameter("email")
+				.permitAll()
+				.and()
+				.logout()
+				.logoutUrl("/logout")
+				.deleteCookies("remember-me")
+				.logoutSuccessUrl("/")
+				.permitAll()
+				.and()
+				.rememberMe();
+	}
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+				.userDetailsService(userDetailsService)
+				.passwordEncoder(new BCryptPasswordEncoder());
+	}
+	
 }
