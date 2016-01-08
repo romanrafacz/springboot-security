460ebe62f99b7feb03a38404f55256faa40f3a4e
shoemonkey <romanr32@gmail.com>
2016-01-07 19:48:24 -0600

    home page login

--- src/main/java/springboot/security/config/SecurityConfig.java
+++ src/main/java/springboot/security/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package springboot.security.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -13,5 +14,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		auth
 			.inMemoryAuthentication()
 				.withUser("admin").password("password").roles("ADMIN");
+	}
+	
+	@Override
+	public void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.authorizeRequests().antMatchers("/").permitAll();
 	}
 }
