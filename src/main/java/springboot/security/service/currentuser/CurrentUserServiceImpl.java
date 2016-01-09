9af2767daadf46ead8aba323a3e0598f27452f41
shoemonkey <romanr32@gmail.com>
2016-01-09 09:40:50 -0600

    CurrentUserServices implemented

--- src/main/java/springboot/security/service/currentuser/CurrentUserServiceImpl.java
+++ src/main/java/springboot/security/service/currentuser/CurrentUserServiceImpl.java
@@ -3,11 +3,19 @@ package springboot.security.service.currentuser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import springboot.security.domain.CurrentUser;
+import springboot.security.domain.Role;
+
 public class CurrentUserServiceImpl implements CurrentUserService {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(CurrentUserServiceImpl.class);
 	
 	@Override
+	public boolean canAccessUser(CurrentUser currentUser, Long userid){
+		LOGGER.debug("Check if user={} has access to current user={}", currentUser, userid);
+		return currentUser != null
+				&& (currentUser.getRole() == Role.ADMIN || currentUser.getId().equals(userid));
+	}
 	
 	
 }
