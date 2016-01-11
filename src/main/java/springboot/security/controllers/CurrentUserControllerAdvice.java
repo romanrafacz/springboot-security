ae09b045e7ae37bb0f977c27e38cac6d2514913d
shoemonkey <romanr32@gmail.com>
2016-01-10 18:28:08 -0600

    db added in resources

--- src/main/java/springboot/security/controllers/CurrentUserControllerAdvice.java
+++ src/main/java/springboot/security/controllers/CurrentUserControllerAdvice.java
@@ -2,6 +2,7 @@ package springboot.security.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AuthenticationTrustResolverImpl;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,7 +15,7 @@ public class CurrentUserControllerAdvice {
 	private static final Logger LOGGER = LoggerFactory.getLogger(CurrentUserControllerAdvice.class);
 
 	@ModelAttribute("currentUser")
-	public CurrentUser getCurrentUser(Authentication auth){
-		return (auth == null) ? null : (CurrentUser) auth.getPrincipal();
+	public CurrentUser getCurrentUser(Authentication authentication){
+		return (authentication == null) ? null : (CurrentUser) authentication.getPrincipal();
 	}
 }
